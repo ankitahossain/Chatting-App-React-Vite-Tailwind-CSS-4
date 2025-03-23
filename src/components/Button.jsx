3709a44638fbf04149461489be1d9510b76e4fb2
ankitahossain <ankitahossaina@gmail.com>
2025-03-24 01:16:09 +0600

    code added

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({design,onClick,content}) => {
+  return (
+    <div>
+      <button className={design} onClick ={onClick}>{content}</button>
+    </div>
+  )
+}
+
+export default Button
